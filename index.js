610e7259322e4511a2a1e71f009564603e13c9d5
TAJINDER2 <tajinder14333singh@gmail.com>
2021-05-31 11:52:44 +0530

    website

---
+++ index.js
@@ -0,0 +1,10 @@
+const myfunction=()=>{
+    var navbar=document.getElementById("nav-links");
+    if(navbar.style.display ==="block"){
+        navbar.style.display="none";
+    }
+    else{
+    navbar.style.display="block";
+    }
+
+}
